341e2fe7c832a13b5747d2e6bc7b0d4d8754f0a0
Jonathan SIMONNEY <jonathan.simonney@supinternet.fr>
2018-06-19 16:50:10 +0200

    added the hello world app in app folder.

---
+++ app/app/src/main/java/jrml/supinternet/com/jeredecouvremalangue/SingleWordActivity.kt
@@ -0,0 +1,12 @@
+package jrml.supinternet.com.jeredecouvremalangue
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SingleWordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_word)
+    }
+}
