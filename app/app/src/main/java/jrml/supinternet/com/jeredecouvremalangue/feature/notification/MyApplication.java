78ab081fccb26275d950a01072ad5157b5414c87
Jonathan SIMONNEY <jonathan.simonney@supinternet.fr>
2018-07-01 16:23:53 +0200

    notification working, misses service and better visual / wording.

--- app/app/src/main/java/jrml/supinternet/com/jeredecouvremalangue/feature/notification/MyApplication.java
+++ app/app/src/main/java/jrml/supinternet/com/jeredecouvremalangue/feature/notification/MyApplication.java
@@ -2,13 +2,18 @@ package jrml.supinternet.com.jeredecouvremalangue.feature.notification;
 
 import android.app.AlarmManager;
 import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.util.Log;
 
 import java.util.Calendar;
 import java.util.TimeZone;
+
+import jrml.supinternet.com.jeredecouvremalangue.R;
 
 public class MyApplication extends Application {
     public MyApplication(){
@@ -19,7 +24,25 @@ public class MyApplication extends Application {
         super.onCreate();
         Context context = getApplicationContext();
         setRecurringAlarm(context);
+        createNotificationChannel();
     }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(getString(R.string.channel_id), name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
 
     private void setRecurringAlarm(Context context) {
 
--- app/app/src/main/java/jrml/supinternet/com/jeredecouvremalangue/feature/notification/NotificationReceiver.kt
+++ app/app/src/main/java/jrml/supinternet/com/jeredecouvremalangue/feature/notification/NotificationReceiver.kt
@@ -4,12 +4,47 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import android.support.v4.app.NotificationCompat
+import jrml.supinternet.com.jeredecouvremalangue.R
+import android.app.PendingIntent
+import jrml.supinternet.com.jeredecouvremalangue.feature.word.SingleWordActivity
+import android.support.v4.app.NotificationManagerCompat
+
+
 
 
 class NotificationReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         Log.d("JS\\", "recurring notification, requesting notification service")
-        //todo show the notification
+
+        val pendingIntent = getActivityIntent(context)
+
+        val mBuilder = NotificationCompat.Builder(context, context.getString(R.string.channel_id))
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("test")
+                .setContentText("toto")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                // Set the intent that will fire when the user taps the notification
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+
+        val notificationManager = NotificationManagerCompat.from(context)
+
+        // notificationId is a unique int for each notification that you must define
+        //currently, there is only one notification, so id isn't important...
+        notificationManager.notify(1, mBuilder.build())
+
+    }
+
+    private fun getActivityIntent(context: Context) :PendingIntent{
+        // Create an explicit intent for an Activity in your app
+        val intent = Intent(context, SingleWordActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        //todo call the word service to get a random word id!
+        intent.putExtra("listId", 1)
+        val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
+        return pendingIntent
+
     }
 }
